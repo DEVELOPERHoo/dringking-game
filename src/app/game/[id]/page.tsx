285231c4417550e83f91f84122c90c7ed1e1eaf3
kks2140501 <kks2140501@naver.com>
2025-07-15 22:03:25 +0900

    간단한 화면구성

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,3 +1,13 @@
+import style from "./page.module.css";
+
 export default async function Page() {
-  return <div>게임1의 페이지입니다.</div>;
+  return (
+    <div className={style.container}>
+      게임1의 페이지입니다.
+      <div>
+        <button>방만들기</button>
+        <button>입장하기</button>
+      </div>
+    </div>
+  );
 }
